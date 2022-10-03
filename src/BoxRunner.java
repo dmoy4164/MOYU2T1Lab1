6b4e3f53bef461508d8b74d7180c5c72aad3bd35
Donnie Moy <donniem@nycstudents.net>
2022-10-03 12:24:16 -0400

    Initial commit

---
+++ src/BoxRunner.java
@@ -0,0 +1,6 @@
+public class BoxRunner {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/RectangleRunner.java
@@ -0,0 +1,8 @@
+public class RectangleRunner{
+    int len = 65;
+
+    int wid = len + 10;
+    Rectangle rect1 = new Rectangle(len, wid);
+    Rectangle rect3 = new Rectangle(0);
+}
+
